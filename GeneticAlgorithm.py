1e56aae49c1f2a1036e8a74fb78ddbb8276861e1
Mahdihma <mahdi.haji@yahoo.com>
2022-07-28 17:23:56 +0430

    Implement: Selection functionality => calculate genome fitness and population prob needed

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,8 +1,16 @@
 import time
 import sys
+import random
 from genome import Genome
 
 class GeneticAlgorithm:
+    POPULATION_SIZE = 40
+    MAX_VALUE = 1000000000
+    REMAIN_DESCENDANT_SIZE_PER_POPULATION = 0.2
+
+    best_genome = Genome()
+    best_genome.fitness = MAX_VALUE
+
     def __init__(self, initial_strings, strings_length):
         self.initial_strings = initial_strings
         self.strings_length = strings_length
@@ -26,10 +34,30 @@ class GeneticAlgorithm:
         print("Number of descendants:", descendants, file=sys.stderr)
     
     def __selection(self):
+        self.__calc_genomes_fitness()
+        self.population.sort()
+        del self.population[int(GeneticAlgorithm.REMAIN_DESCENDANT_SIZE_PER_POPULATION * len(self.population)):]
+        if self.population[0].fitness < GeneticAlgorithm.best_genome.fitness:
+            GeneticAlgorithm.best_genome = self.population[0]
+            GeneticAlgorithm.print_best_genome(sys.stderr)
+        probabilities = self.__calc_population_probability()
+        self.population = random.choices(self.population, probabilities, k=GeneticAlgorithm.POPULATION_SIZE)
+
+    def __calc_genomes_fitness(self):
         pass
 
+    def __calc_population_probability(self):
+        pass 
+    
     def __cross_over(self):
         pass
-
+    
     def __mutation(self):
         pass
+
+    @staticmethod
+    def print_best_genome(stdout_or_err):
+        print(GeneticAlgorithm.best_genome.fitness, file=stdout_or_err)
+        for string in GeneticAlgorithm.best_genome.genome:
+            print(string, file=stdout_or_err)
+        print(file=stdout_or_err)
