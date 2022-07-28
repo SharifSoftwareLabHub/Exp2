249689c8512dfe2865bb1e829da4ecced49b6a50
97102044 <ah.abbasi.ah@gmail.com>
2022-07-28 15:23:35 +0430

    add func for put dash in input string

--- genome.py
+++ genome.py
@@ -1,2 +1,21 @@
+import random
+
+
 class Genome:
-    pass
+    def __init__(self):
+        self.genome = None
+        self.fitness = None
+
+    @staticmethod
+    def put_dashes_and_get_final_string(dashes_indexes, string, strings_length):
+        chars = []
+        string_counter = 0
+        dashes_indexes_counter = 0
+        for i in range(strings_length):
+            if dashes_indexes_counter < len(dashes_indexes) and i == dashes_indexes[dashes_indexes_counter]:
+                chars.append('-')
+                dashes_indexes_counter += 1
+            else:
+                chars.append(string[string_counter])
+                string_counter += 1
+        return ''.join(chars)
