b53248e143cb245794959857dd0b912c05b09a1d
97102044 <ah.abbasi.ah@gmail.com>
2022-07-29 14:21:16 +0430

    add: init def for controller

--- controller.py
+++ controller.py
@@ -1,3 +1,13 @@
 class Controller:
+    MAX_GENOMES_STRINGS_LENGTH_INCREMENT_FOR_GA = 5
+    instance = None
+
     def __init__(self):
-        pass
+        self.alphabet_size = 0
+        self.alphabet = []
+        self.number_of_strings = None
+        self.initial_strings = []
+        self.conversion_cost = None
+        self.matching_cost = []
+        self.max_strings_length_index = None
+        self.max_strings_length = None
