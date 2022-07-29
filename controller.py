5fdcd83cd1d5261b0b38e584581f4fe8e95bdd22
Mahdihma <mahdi.haji@yahoo.com>
2022-07-29 14:32:37 +0430

    Implement: Start Controller Function

--- controller.py
+++ controller.py
@@ -1,3 +1,6 @@
+from  GeneticAlgorithm import GeneticAlgorithm
+import sys
+
 class Controller:
     MAX_GENOMES_STRINGS_LENGTH_INCREMENT_FOR_GA = 5
     instance = None
@@ -11,3 +14,23 @@ class Controller:
         self.matching_cost = []
         self.max_strings_length_index = None
         self.max_strings_length = None
+
+    def start(self):
+            self.__get_inputs()
+            self.__calc_max_string_length_index()
+            for strings_length in range(self.max_strings_length,
+                                        self.max_strings_length + Controller.MAX_GENOMES_STRINGS_LENGTH_INCREMENT_FOR_GA):
+                ga = GeneticAlgorithm(self.initial_strings, strings_length, self.matching_cost,
+                        self.alphabet, self.__calc_initial_conversion_cost(strings_length), self.max_strings_length_index)
+                ga.start(60)    # algorithm run's time for specific strings_length
+                print("\nSTRINGS LENGTH FOR GA INCREMENTED\n", file=sys.stderr)
+            GeneticAlgorithm.print_best_genome(sys.stdout)
+    
+    def __get_inputs(self):
+        pass
+
+    def __calc_max_string_length_index(self):
+        pass
+
+    def __calc_initial_conversion_cost(self, strings_length):
+        pass
